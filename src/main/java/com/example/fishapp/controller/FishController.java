220b669f20d3df03a6fd7e96ab45959e25ba51c3
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-06-04 12:24:03 +0900

    編集機能を実装

--- src/main/java/com/example/fishapp/controller/FishController.java
+++ src/main/java/com/example/fishapp/controller/FishController.java
@@ -2,14 +2,17 @@ package com.example.fishapp.controller;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import com.example.fishapp.model.Fish;
 import com.example.fishapp.repository.FishRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 
@@ -44,6 +47,32 @@ public class FishController {
         fish.setReview(review);
         fish.setHistory(history);
         fishRepository.save(fish);
+        
+        return "redirect:/fish";
+    }
+    
+    @GetMapping("/fish/edit/{id}")
+        public String showEditForm(@PathVariable Long id, Model model) {
+        Fish fish = fishRepository.findById(id);  // idで検索
+        model.addAttribute("fish", fish);
+        return "fish-edit"; // 編集用のHTML
+}
+
+    @PostMapping("/fish/update")
+    public String updateFish(
+        @RequestParam Long id,
+        @RequestParam String name,
+        @RequestParam int price,
+        @RequestParam(required = false) String feature,
+        @RequestParam(required = false) String review,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate history
+        ) {
+        Fish fish = fishRepository.findById(id);
+        fish.setName(name);
+        fish.setPrice(price);
+        fish.setFeature(feature);
+        fish.setReview(review);
+        fish.setHistory(history);
         
         return "redirect:/fish";
     }
--- src/main/java/com/example/fishapp/repository/FishRepository.java
+++ src/main/java/com/example/fishapp/repository/FishRepository.java
@@ -5,12 +5,25 @@ import java.util.List;
 
 public class FishRepository {
     private List<Fish> fishList = new ArrayList<>();
+    private Long nextId = 1L; 
 
     public List<Fish> findAll() {
         return fishList;
     }
 
+    public Fish findById(Long id) {
+        for (Fish fish : fishList) {
+            if (fish.getId() !=null && fish.getId().equals(id)) {
+                return fish;
+            }
+        }
+        return null;
+    }
+
     public void save(Fish fish) {
+        if (fish.getId() == null) {
+            fish.setId(nextId++);
+        }
         fishList.add(fish);
     }
 }
